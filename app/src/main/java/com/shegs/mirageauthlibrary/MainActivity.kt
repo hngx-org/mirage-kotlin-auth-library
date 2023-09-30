f5170214a2336a0d2cc8e8534186b1780bbf25a3
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-09-30 23:16:09 +0100

    Revert "datastore setup and login logic"

--- app/src/main/java/com/shegs/mirageauthlibrary/MainActivity.kt
+++ app/src/main/java/com/shegs/mirageauthlibrary/MainActivity.kt
@@ -1,43 +1,23 @@
 package com.shegs.mirageauthlibrary
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.shegs.hng_auth_library.authlibrary.AuthLibrary
-import com.shegs.hng_auth_library.model.LoginRequest
-import com.shegs.hng_auth_library.network.ApiResponse
+import com.shegs.hng_auth_library.ui.screens.LibraryButton
 import com.shegs.mirageauthlibrary.ui.theme.MirageAuthLibraryTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -54,150 +34,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    val authService = AuthLibrary.createAuthService()
-    val signupRepository = AuthLibrary.createSignupRepository(authService)
-    val dataStoreRepository = AuthLibrary.createDataStoreRepository(context = LocalContext.current)
-    val loginRepository = AuthLibrary.createLoginRepository(authService, dataStoreRepository)
-
-
-    var name by remember { mutableStateOf("") }
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    var confirm_password by remember { mutableStateOf("") }
-
     Box(
         modifier = Modifier
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Column(
-            modifier = Modifier
-                .padding(12.dp),
-            verticalArrangement = Arrangement.Center,
+            modifier = Modifier,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
-            TextField(
-                value = name,
-                label = {
-                    Text(text = "Name")
-                },
-                onValueChange = { name = it },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = androidx.compose.ui.text.input.ImeAction.Next
-                ),
-                keyboardActions = KeyboardActions(
-                    onNext = { /* Focus on the next field */ }
-                )
+            Text(
+                text = "$name!",
+                modifier = modifier
             )
-
-            TextField(
-                value = email,
-                label = {
-                        Text(text = "Email")
-                },
-                onValueChange = { email = it },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = androidx.compose.ui.text.input.ImeAction.Next
-                ),
-                keyboardActions = KeyboardActions(
-                    onNext = { /* Focus on the next field */ }
-                )
+            LibraryButton(
+                onClick = {},
+                texts = "Checking",
             )
-
-            TextField(
-                value = password,
-                label = {
-                    Text(text = "Password")
-                },
-                onValueChange = { password = it },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = androidx.compose.ui.text.input.ImeAction.Next
-                ),
-                keyboardActions = KeyboardActions(
-                    onNext = { /* Focus on the next field */ }
-                )
-            )
-
-            TextField(
-                value = confirm_password,
-                label = {
-                    Text(text = "Confirm password")
-                },
-                onValueChange = { confirm_password = it },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = androidx.compose.ui.text.input.ImeAction.Next
-                ),
-                keyboardActions = KeyboardActions(
-                    onNext = { /* Focus on the next field */ }
-                )
-            )
-
-            val coroutineScope = rememberCoroutineScope()
-            val context = LocalContext.current
-
-            Button(
-                onClick = {
-                    // This code will be executed when the button is clicked
-                    coroutineScope.launch {
-//                        val result = signupRepository.signup(
-//                            SignupRequest(
-//                                name = name,
-//                                email = email,
-//                                password = password,
-//                                confirm_password = confirm_password
-//                            )
-//                        )
-
-                        val result = loginRepository.login(
-                            LoginRequest(
-                                email = email,
-                                password = password,
-                            )
-                        )
-
-
-
-
-
-                        when (result) {
-                            is ApiResponse.Success -> {
-                                // Handle successful signup
-                                val user = result
-                                Log.d("login succ",user.toString())
-//                                Toast.makeText(context, "Signup successful: ${user.data.name}", Toast.LENGTH_SHORT).show()
-                            }
-
-                            is ApiResponse.Error -> {
-                                // Handle signup error
-                                val errorMessage = result.message
-                                Log.d("login err",errorMessage)
-                                // Display error message to the user
-                                Toast.makeText(context, "Signup successful: $errorMessage", Toast.LENGTH_SHORT).show()
-                            }
-                        }
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                Text("Sign Up")
-            }
-
-
-            //SignInScreen()
         }
     }
 }
--- hng-auth-library/src/main/java/com/shegs/hng_auth_library/model/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.shegs.hng_auth_library.model
-
-data class LoginRequest(
-    val email:String,
-    val password:String,
-)
