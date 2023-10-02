bce8b55ba2068f1cb404e03fdc50fa195252ec34
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-10-02 13:52:42 +0100

    gradle issues resolved

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20") // Update to the latest version
+        classpath ("org.jetbrains.kotlin:kotlin-annotation-processing-gradle:1.8.20") // Update to the latest version
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id ("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id ("com.google.dagger.hilt.android") version "2.45" apply false
 }
